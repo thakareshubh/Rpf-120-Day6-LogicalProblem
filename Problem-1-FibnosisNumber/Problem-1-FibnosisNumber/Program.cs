b5ef8f9217521ff8e951f91c2222984d13ac3fa4
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 16:51:18 +0530

    Creat fibnosis number project

---
+++ Problem-1-FibnosisNumber/Problem-1-FibnosisNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem_1_FibnosisNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number=Convert.ToInt32(Console.ReadLine());
+            FibnoNumber p1 = new FibnoNumber();
+            p1.Fibno(number);
+
+        }
+    }
+    public class FibnoNumber
+    {
+        public void Fibno(int number)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3;
+
+            Console.Write(n1+" ");
+            for(int i = 0; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3+" ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
