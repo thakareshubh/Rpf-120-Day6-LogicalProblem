1f05afeb5816bcf874168dce1f159b27825a5f11
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 16:53:29 +0530

    creat perfect number project

---
+++ Problem-2-PerfectNumber/Problem-2-PerfectNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Problem_2_PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int number =Convert.ToInt32(Console.ReadLine());
+            PerfectNumber p1 = new PerfectNumber();
+            p1.Perfect(number);
+        }
+    }
+    public class PerfectNumber
+    {
+        public void Perfect(int number)
+        {
+            int temp = 0;
+            for(int i = 1; i < number; i++)
+            {
+                if(number%i==0)
+                {
+                    temp=temp+i;
+                }
+                
+            }
+            if(temp==number)
+            {
+                Console.WriteLine(number + " is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not perfect number");
+            }
+        }
+    }
+}
