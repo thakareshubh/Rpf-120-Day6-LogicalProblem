65c613efa8c1f4967a02d167ec2060270234c788
thakareshubh <shubhamthakare032@gmail.com>
2022-03-10 16:56:55 +0530

    creat reverse number project

---
+++ Problem-4-ReversNumber/Problem-4-ReversNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem_4_ReversNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter more than four digit number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ReverseNumber p1 = new ReverseNumber();
+            p1.Reverse(number);
+        }
+    }
+
+    public class ReverseNumber
+    {
+        public void Reverse(int number)
+        {
+            int baki,  revers=0;
+            while(number!=0)
+            {
+                baki = number % 10;
+                revers = revers * 10 + baki;
+                number = number / 10;
+            }
+            Console.WriteLine(revers);
+        }
+    }
+}
